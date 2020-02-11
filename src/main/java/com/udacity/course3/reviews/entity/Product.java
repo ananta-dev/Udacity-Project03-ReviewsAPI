c80a1c303effc7092d725d842f80a68752c3cb41
freenrg <juan.jose.guirao@gmail.com>
2020-02-11 14:25:48 +0000

    Updated product.java

--- src/main/java/com/udacity/course3/reviews/entity/Product.java
+++ src/main/java/com/udacity/course3/reviews/entity/Product.java
@@ -16,10 +16,47 @@ public class Product {
     private String productName;
 
     @Column(name = "product_desc")
-    private String productDescription;
+    private String productDesc;
 
     @OneToMany(mappedBy = "product")
     @JsonManageReference
     private List<Review> reviews = new ArrayList<>();
+    
+    public Product(String productName, String productDesc) {
+        this.productName = productName;
+        this productDesc = productDesc);
+    }
+    
+    public Long getProductId() {
+        return productId;
+    }
 
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+    
 }
